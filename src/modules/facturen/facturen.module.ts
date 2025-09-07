5ae992b6b9f5e3c7f23663b5440c31e6015354ec
Stein Jonker <stein.jonker@eraze.com>
2025-09-07 12:19:31 +0200

    Fix not importing required modules for facturen.module.ts

--- src/modules/facturen/facturen.module.ts
+++ src/modules/facturen/facturen.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FacturenService } from './facturen.service';
 import { FacturenController } from './facturen.controller';
+import {HeliosCoreModule} from "../../core/helios.core.module";
+import {AuthorisatieModule} from "../authorisatie/authorisatie.module";
 
 @Module({
   providers: [FacturenService],
+  imports: [HeliosCoreModule, AuthorisatieModule],
   controllers: [FacturenController]
 })
 export class FacturenModule {}
