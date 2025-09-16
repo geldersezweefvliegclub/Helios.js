8645153ca240bdfe7c2126b3abaaf86b5601cb20
Staijn1 <stein@jnkr.eu>
2025-09-16 19:32:43 +0200

    Annotate swagger objects correctly

--- src/modules/dag-info/GetObjectOperDagInfoRequest.ts
+++ src/modules/dag-info/GetObjectOperDagInfoRequest.ts
@@ -1,5 +1,6 @@
 import {Transform} from "class-transformer";
 import {IsDate, IsInt, IsOptional} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
 
 /**
  * DTO class for requesting a SINGLE OperDagInfo object
@@ -7,10 +8,17 @@ import {IsDate, IsInt, IsOptional} from "class-validator";
 export class GetObjectOperDagInfoRequest {
     @IsOptional()
     @IsInt()
+    @ApiProperty({name: "ID", required: false, type: Number})
     ID?: number;
 
-    @Transform(({ value }) =>  new Date(value))
+    @Transform(({value}) => new Date(value))
     @IsDate()
     @IsOptional()
+    @ApiProperty(
+        {
+            name: 'DATUM',
+            required: false,
+            type: Date
+        })
     public DATUM: Date;
 }
