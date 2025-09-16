14eea811404aeee6978f9fb3d72d3ad8616dfc07
Staijn1 <stein@jnkr.eu>
2025-09-16 20:35:16 +0200

    Fix Documenten/GetObjects throwing HTTP 400 bad request because integers are not parsed

--- src/modules/documenten/GetObjectsHeliosDocumentenRequest.ts
+++ src/modules/documenten/GetObjectsHeliosDocumentenRequest.ts
@@ -1,26 +1,32 @@
 import {GetObjectsRequest} from "../../core/DTO/IHeliosFilter";
-import {IsOptional} from "class-validator";
+import {IsInt, IsOptional} from "class-validator";
 import {ApiProperty} from "@nestjs/swagger";
+import {OptionalNumberTransform} from "../../core/helpers/Transformers";
 
-export class GetObjectsHeliosDocumentenRequest extends GetObjectsRequest
-{
-   // hier komen de specifieke velden voor GetObjects
-   @IsOptional()
-   @ApiProperty(
-      {
-         required: false,
-         description: 'Opvragen documenten van een specifiek lid',
-         type: String
-      })
-   LID_ID?:  number;
+export class GetObjectsHeliosDocumentenRequest extends GetObjectsRequest {
+    // hier komen de specifieke velden voor GetObjects
+    @IsOptional()
+    @OptionalNumberTransform()
+    @IsInt()
+    @ApiProperty(
+        {
+            name: "LID_ID",
+            required: false,
+            description: 'Opvragen documenten van een specifiek lid',
+            type: Number
+        })
+    LID_ID?: number;
 
-   @IsOptional()
-   @ApiProperty(
-      {
-         required: false,
-         description: 'Opvragen documenten uit een specifieke groep',
-         type: String
-      })
-   GROEP_ID?:  number;
+    @IsOptional()
+    @IsInt()
+    @OptionalNumberTransform()
+    @ApiProperty(
+        {
+            name: "GROEP_ID",
+            required: false,
+            description: 'Opvragen documenten uit een specifieke groep',
+            type: Number
+        })
+    GROEP_ID?: number;
 }
 
